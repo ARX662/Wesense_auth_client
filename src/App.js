659a7aa61bd7ef369f903893f49300098306cb8b
arathtemp <arath04@qub.ac.uk>
2023-09-24 17:17:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React,{ useState  } from 'react';
+import { useEffect } from "react";
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
+import { Home, Landing, Login, Register } from "C:/Users/LEGION/Desktop/wesense authentication/client/src";
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+  
+  useEffect(() => {
+    const theUser = localStorage.getItem("user");
+
+    if (theUser && !theUser.includes("undefined")) {
+      setUser(JSON.parse(theUser));
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route  path="/"
+         element={user?.email ? <Navigate to="/home" /> : <Landing />}/>
+
+      <Route
+      path="/Register"
+      element={user?.email ? <Navigate to="/home" /> : <Register/>}
+      />
+      <Route
+        path="/login"
+        element={user?.email ? <Navigate to="/home" /> : <Login />}
+      />
+          <Route
+              path="/home"
+              element={user?.email ? <Home user={user} /> : <Navigate to="/" />}
+           />
+           </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
